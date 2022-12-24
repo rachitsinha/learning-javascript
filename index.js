f2c4c6051e78464f0ef418ecde8cd084bce75dff
Rachit Sinha <rachitsinha123@gmail.com>
2022-12-25 00:11:37 +0100

    playing with functions

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-document.getElementById('countElem').innerText = 5;
+function myfn(a=90, b=100) {
+    console.log('myfn start');
+    console.log(`a = ${a} and b = ${b}`);
+    console.log('myfn end');
+}
+// myfn(10,2);
+// myfn(21);
+// myfn('hello world');
+// myfn({fn: 'rachit', ln: 'sinha'});
+// myfn();
+
+
+function mysecondfn(a,b,c) {
+    console.log(mysecondfn.caller);
+}
+
+function mythirdfn() {
+    mysecondfn(10, false, 99);
+}
+
+mythirdfn();
+
